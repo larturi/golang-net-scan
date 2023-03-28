08ffdbe2451c803044afb6414ff6ed4dc06bfde5
Leandro Arturi <lea.arturi@gmail.com>
2023-03-28 17:42:57 -0300

    Initial commit

---
+++ port.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+)
+
+var site = flag.String("site", "scanme.nmap.org", "URL to scan")
+
+func main() {
+
+	// Ej: go run port.go --site=scanme.nmap.org
+
+	flag.Parse()
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+
+		go func(port int) {
+			defer wg.Done()
+
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+
+			if err != nil {
+				return
+			}
+
+			conn.Close()
+
+			fmt.Printf("Port %d is open\n", port)
+		}(i)
+
+	}
+
+	wg.Wait()
+
+	fmt.Println("Scan completed")
+}
